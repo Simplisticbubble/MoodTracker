dfb127814a60b6e99a9b8a5b6ee5546cf5beaa55
Simplisticbubble <ks.kevinseu@gmail.com>
2025-06-13 21:04:31 +1000

    changed serv er url

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,10 +11,13 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/login", {
-        username,
-        password,
-      });
+      const res = await axios.post(
+        "https://natural-victory-production.up.railway.app/login",
+        {
+          username,
+          password,
+        }
+      );
 
       if (res.data.token) {
         localStorage.setItem("token", res.data.token);
